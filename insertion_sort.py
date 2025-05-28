04e923c6f8b2ebf3ea86510b923108e95a55b538
Nicholas Blaisdell <nblaisdell2024@my.fit.edu>
2025-05-28 16:27:06 -0400

    Update insertion_sort.py
    Current version of assignment for lab due time

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,12 +1,22 @@
 # Python program for implementation of Insertion Sort
  
 # Function to do insertion sort
-def insertionSort(arr):
-  # TODO 
- 
- 
+def insertionSort(A):
+  count = 1
+  for j in range(1, len(A)):
+    key = A[j]
+    i = j - 1
+    while i >= 0 and A[i] > key:
+      A[i + 1] = A[i]
+      i = i - 1
+    A[i + 1] = key
+    print(f"{count}. {A}")
+    count += 1
+
 # Driver code to test above
 arr = [12, 11, 13, 5, 6]
+print("Initial Array: ", arr)
 insertionSort(arr)
+print("Final result: ", arr)
 for i in range(len(arr)):
     print ("% d" % arr[i])
